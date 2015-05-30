42c450a638ae1184e41828944ff4f767f7316362
ctrevarthen <ct@detroitlabs.com>
2015-05-29 22:03:38 -0400

    Add OHHTTPStub test for API call with a mock data fixture.

--- SunnyDazeTests/WeatherServiceSpec.m
+++ SunnyDazeTests/WeatherServiceSpec.m
@@ -7,6 +7,8 @@
 //
 
 #import <Kiwi/Kiwi.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
+
 #import "WeatherService.h"
 
 @interface WeatherService (Spec)
@@ -19,23 +21,67 @@ SPEC_BEGIN(WeatherServiceSpec)
 
     describe(@"WeatherService", ^{
 		
+        beforeEach(^{
+            NSBundle *mainBundleMock = [NSBundle nullMock];
+            [NSBundle stub:@selector(mainBundle) andReturn:mainBundleMock];
+            [mainBundleMock stub:@selector(objectForInfoDictionaryKey:) andReturn:@"12345" withArguments:@"weatherUndergroundAPIKey"];
+        });
+        
         let(weatherService, ^WeatherService *{
             return [WeatherService new];
         });
         
         describe(@"#weatherUrlStringFromCity:andState:", ^{
             
-            context(@"it returns a URL string with the city and state", ^{
-               
-                NSBundle *mainBundleMock = [NSBundle nullMock];
-                [NSBundle stub:@selector(mainBundle) andReturn:mainBundleMock];
-                [mainBundleMock stub:@selector(objectForInfoDictionaryKey:) andReturn:@"12345" withArguments:@"weatherUndergroundAPIKey"];
+            it(@"returns a URL string with the city and state", ^{
                 
                 NSString *actualURLString = [weatherService weatherUrlStringFromCity:@"San Francisco" andState:@"CA"];
                 
                 [[actualURLString should] equal:@"http://api.wunderground.com/api/12345/conditions/q/CA/San_Francisco.json"];
                 
+            });
+            
+        });
+        
+        context(@"when API call is successful", ^{
+           
+            beforeAll(^{
                 
+                [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                    
+                    return [request.URL.absoluteString isEqualToString:@"http://api.wunderground.com/api/12345/conditions/q/CA/San_Francisco.json"];
+                    
+                } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+                    
+                    // Stub it with our "weather.json" stub file (which is in same bundle as self)
+                    NSString* fixture = OHPathForFile(@"weather.json", self.class);
+                    return [OHHTTPStubsResponse responseWithFileAtPath:fixture
+                                                            statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+                    
+                }];
+                
+            });
+            
+            it(@"calls the success block with weather dictionary", ^{
+                
+                __block NSDictionary *weatherResponse;
+                
+                NSString *city = @"San Francisco";
+                NSString *stateAbbreviation = @"CA";
+                
+                [weatherService searchByCity:city andState:stateAbbreviation withSuccess:^(NSDictionary *weather) {
+                
+                    weatherResponse = weather;
+                    
+                }];
+                
+                [[expectFutureValue(weatherResponse[@"current_observation"][@"display_location"][@"full"]) shouldEventually] equal:@"San Francisco, CA"];
+                
+            });
+            
+            afterAll(^{
+               
+                [OHHTTPStubs removeAllStubs];
                 
             });
             
